eb62c02d89f39fcf913f2b5d8a8a14ec1fad815f
RobsonDaniel <Robson-Daniel@outlook.com>
2017-10-01 00:36:46 -0300

    animação height do menu adicionado

--- js/animarBtnMenu.js
+++ js/animarBtnMenu.js
@@ -1,11 +1,17 @@
-function menu(btn_menu) {
-    btn_menu.classList.toggle("animarBtnMenu");
+var btn_menu = document.getElementsByClassName("btn-menu")[0],
+    menu_conteudo = document.getElementById("menu-conteudo");
 
-    var menu_conteudo = document.getElementById("menu-conteudo");
+function animarBtnMenu() {
+  btn_menu.classList.toggle("animarBtnMenu");
+}
 
-    if (menu_conteudo.style.display === "block") {
-      menu_conteudo.style.display = "none";
+function menu() {
+    animarBtnMenu();
+
+    if (menu_conteudo.style.maxHeight) {
+      menu_conteudo.style.maxHeight = null;
     }else {
-      menu_conteudo.style.display = "block";
+      menu_conteudo.style.maxHeight = menu_conteudo.scrollHeight + "px";
     }
+
 }
